9f8805993e8c873a3a39f64746f4152d68fe3862
sutriptaroywork <sutriptaroywork@gmail.com>
2023-04-28 13:44:37 +0530

    added lodash checking

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+const _ = require('lodash')
+
 function isSutripta(str) {
     return str === "Sutripta"
 }
 
+function checkString(value) {
+    return _.isString(value)
+}
+
 module.exports = isSutripta
+module.exports = checkString
